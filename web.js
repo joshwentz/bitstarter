d06f2562b1ae19bed29fe26d96543e7e13f2c24a
Josh Wentz <joshwentz@gmail.com>
2013-07-12 03:16:31 +0000

    add index

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var buf = new Buffer(256);
+var index = fs.readFileSync(index.html);
+var buffer = buf.toString('utf8', 0, buffer.length);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
