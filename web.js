b087842e060b3c604b51c9024158e04ed88c1027
Josh Wentz <joshwentz@gmail.com>
2013-07-12 03:34:36 +0000

    index4

--- web.js
+++ web.js
@@ -3,20 +3,13 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-
-
 // store the contents of 'index.html' to a buffer
-var index = fs.readFileSync('./index.html', 'utf8');
-
-var buf = new Buffer(256);
-var buffer = buf.toString('utf8', 0, buffer.length);
+var buf = fs.readFileSync('./index.html', 'utf8');
+var index = buf.toString();
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-  //response.send(index);
-  
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.end(index);
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
